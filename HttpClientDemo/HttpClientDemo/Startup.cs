0320335ad3972ba0270df665b65f83a7e9568334
patrick <patrick204nqh@gmail.com>
2020-06-15 14:50:54 +0700

    add uri into Startup.cs

--- HttpClientDemo/HttpClientDemo/Startup.cs
+++ HttpClientDemo/HttpClientDemo/Startup.cs
@@ -26,6 +26,7 @@ namespace HttpClientDemo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            string uri = Configuration.GetValue<string>("MetaAPI");
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
